ff6e1240c3fa266f10c0bb0b1ade8425a0dd7930
Linus Hedenberg <linushedenberg@gmail.com>
2013-09-11 17:31:38 +0200

    Added a simple file download

--- websync/views.py
+++ websync/views.py
@@ -1,5 +1,6 @@
+import os
 from websync import app
-from flask import redirect, request, url_for, render_template
+from flask import redirect, request, url_for, render_template, make_response
 from werkzeug import secure_filename
 from websync.database import db_session
 from websync.models import Blob
@@ -31,7 +32,22 @@ def blob_redirect():
 @app.route('/blob/<int:blob_id>', methods=['GET', 'PUT', 'DELETE'])
 def show_user_profile(blob_id):
     if request.method == 'GET':
-        return 'Blob resource id %s' % blob_id
+        blob = db_session.query(Blob).get(blob_id)
+        print "Blobid: %d" % blob_id
+        print "Blob: id:%d, filename:%s" % (blob.id, blob.filename)
+        # tell nginx to server the file and where to find it
+        file_size = os.path.getsize('websync/blobs/'+blob.filename)
+        print file_size
+        response = make_response()
+        response.headers['Pragma'] = 'public'
+        response.headers['Content-Type'] = 'txt'
+        response.headers['Content-Transfer-Encoding'] = 'binary'
+        response.headers['Content-Description'] = 'File Transfer'
+        response.headers['Content-Disposition'] = 'attachment; filename=%s' % blob.filename
+        response.headers['Content-Length'] = file_size
+        with open ("websync/blobs/"+blob.filename, "r") as blob_data:
+            response.data = blob_data.read()
+        return response
     elif request.method == 'PUT':
         return "Change that blob"
     elif request.method == 'DELETE':
