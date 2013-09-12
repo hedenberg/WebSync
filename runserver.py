5d53c00155f8b35e3af2e633c66ee61df2357ccf
Linus Hedenberg <linushedenberg@gmail.com>
2013-09-12 09:39:07 +0200

    Added an extremely simple css-file to get some structure, nothing fancy.

--- runserver.py
+++ runserver.py
@@ -1,3 +1,4 @@
 from websync import app
 app.debug = True
+app.secret_key = 'super secret developmentkey, not at all posted on github.'
 app.run()
--- websync/views.py
+++ websync/views.py
@@ -1,10 +1,11 @@
 import os
 from websync import app
-from flask import redirect, request, url_for, render_template, make_response
+from flask import redirect, request, url_for, render_template, make_response, flash
 from werkzeug import secure_filename
 from websync.database import db_session
 from websync.models import Blob
 
+# Removes database session at shutdown
 @app.teardown_appcontext
 def shutdown_session(exception=None):
     db_session.remove()
@@ -16,15 +17,20 @@ def index():
 @app.route('/blob', methods=['GET', 'POST'])
 def blob():
     if request.method == 'GET':
-        return render_template('sampleFileUpload.html', blobs=db_session.query(Blob).order_by(Blob.id))
+        return render_template('show_files.html', blobs=db_session.query(Blob).order_by(Blob.id))
     elif request.method == 'POST':
+        # Adding a new file
         f = request.files['blob']
         fn = secure_filename(f.filename)
+        # Saves the file 
         f.save('websync/blobs/' + fn)
+        # Adds information about the file in the database
         b = Blob(fn)
         db_session.add(b)
         db_session.commit()
-        return "Upload a new blob. name:%s" % fn
+        flash('File upload successful')
+        return render_template('show_files.html', blobs=db_session.query(Blob).order_by(Blob.id))
+
 @app.route('/blob/', methods=['GET', 'POST'])
 def blob_redirect():
     return redirect(url_for('blob'))
@@ -32,12 +38,11 @@ def blob_redirect():
 @app.route('/blob/<int:blob_id>', methods=['GET', 'PUT', 'DELETE'])
 def show_user_profile(blob_id):
     if request.method == 'GET':
+        # Download file!
+        # Retrieve file information from database using id
         blob = db_session.query(Blob).get(blob_id)
-        print "Blobid: %d" % blob_id
-        print "Blob: id:%d, filename:%s" % (blob.id, blob.filename)
-        # tell nginx to server the file and where to find it
+        # Build a response that will return the file to the user
         file_size = os.path.getsize('websync/blobs/'+blob.filename)
-        print file_size
         response = make_response()
         response.headers['Pragma'] = 'public'
         response.headers['Content-Type'] = 'txt'
@@ -45,6 +50,7 @@ def show_user_profile(blob_id):
         response.headers['Content-Description'] = 'File Transfer'
         response.headers['Content-Disposition'] = 'attachment; filename=%s' % blob.filename
         response.headers['Content-Length'] = file_size
+        # Load file data to the response
         with open ("websync/blobs/"+blob.filename, "r") as blob_data:
             response.data = blob_data.read()
         return response
