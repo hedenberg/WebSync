d01dbd09fe55f7fb1bba569bbfd375f14fbe55aa
Linus Hedenberg <linushedenberg@gmail.com>
2013-10-04 15:22:47 +0200

    Added port-argument to runserver

--- runserver.py
+++ runserver.py
@@ -1,9 +1,12 @@
 from websync import app
+import sys
 app.debug = True
-#app.host = '0.0.0.0'
 app.secret_key = 'super secret developmentkey, not at all posted on github.'
 
 port = 80
 if app.debug:
-	port = 5000
+	try:
+		port = int(sys.argv[1])
+	except IndexError:
+		port = 5000
 app.run(host = '0.0.0.0', port=port)
