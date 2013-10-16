161964a5928a7dab83b092d9b1c35045d44b4b5c
Andreas Hägglund <gegga87@gmail.com>
2013-10-16 13:09:56 +0200

    plz work

--- manager/rabbit_combined.py
+++ manager/rabbit_combined.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import pika
 import sys
-from websync.views import manager_receive
+from manager.views import manager_receive
 
 exchange_emit ="Manager" #app.port
 connection_emit = pika.BlockingConnection(pika.ConnectionParameters(
@@ -20,23 +20,7 @@ channel_rec.exchange_declare(exchange=exchange_rec,
 
 ##***************** Manager -> Nodes *************************
 
-def manager_rec_logs(): #Nodes receieves messages from Manager
-    result = channel_emit.queue_declare(exclusive=True)
-    queue_name = result.method.queue
 
-    channel_emit.queue_bind(exchange=exchange_emit,
-                       queue=queue_name)
-
-    print ' [*] Waiting for logs. To exit press CTRL+C'
-    def callback(ch, method, properties, body):
-      print " [x] %r" % (body,)
-
-
-    channel_emit.basic_consume(callback,
-                          queue=queue_name,
-                          no_ack=True)
-
-    channel_emit.start_consuming()
 
 def manager_emit_log(txt):  #Manager sends messages to nodes
     #print exchange_name +"emit"
@@ -67,10 +51,5 @@ def update_rec_logs():  #Manager receives messages from Nodes
 
     channel_rec.start_consuming()
 
-def update_emit_log(txt):  #Nodes sends messages to Manager
-    #print exchange_name +"emit"
-    channel_rec.basic_publish(exchange=exchange_rec,
-                          routing_key='',
-                          body=txt)
-    print " [x] Sent %r" % (txt,)
+
 
