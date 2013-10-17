b6c499559881813c525836195694bb55c4fd93a3
Andreas Hägglund <gegga87@gmail.com>
2013-10-17 21:21:28 +0200

    Can now spawn instances on openstack

---
+++ websync/rabbit_emit.py
@@ -0,0 +1,18 @@
+import pika
+import sys
+#from websync import app
+
+exchange_name ="nodeport8011" #app.port
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+            host='130.240.110.14'))
+
+channel = connection.channel()
+channel.exchange_declare(exchange=exchange_name,
+                         type='fanout')
+
+def emit_log(txt):
+    print exchange_name +"emit"
+    channel.basic_publish(exchange=exchange_name,
+                          routing_key='',
+                          body=txt)
+    print " [x] Sent %r" % (txt,)
