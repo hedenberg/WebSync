88ca22fb91c9e0f23b4e5ae8197c009f4e31dac1
Andreas Hägglund <gegga87@gmail.com>
2013-10-20 13:01:30 +0200

    openstack manager more or less done

--- manager/openstack_manager.py
+++ manager/openstack_manager.py
@@ -4,6 +4,7 @@ import time
 #import config
 
 COREOS_IMAGE = "ami-00000003"
+COREOS_SNAP = "ami-0000001d"
 #COREOS_IMAGE = "56eed2c2-40b6-4a52-bd55-823457f0ee66"
 EC2_ENDPOINT = "130.240.233.106"
 EC2_ACCESS_KEY="58433a1650b14ac2a62a9ad06b9cf1c9"
@@ -22,7 +23,8 @@ def add_instance():
     try:
         response = boto_connection.run_instances(
             #image_id="56eed2c2-40b6-4a52-bd55-823457f0ee66",
-            COREOS_IMAGE, 
+            #COREOS_IMAGE, 
+            COREOS_SNAP,
             key_name="web_sync2", 
             instance_type="m1.tiny", 
             security_groups=["default"]
@@ -44,7 +46,7 @@ def add_instance():
 
 def remove_instance(instances,addr):                                #removes the instance provided
     ip = (str(addr).split(":"))[1]
-    if (boto_connection.disassociate_address(ip)):
+    if (boto_connection.disassociate_address(ip)):                  #disassociate ip from instance before removing to avoid errors
         boto_connection.terminate_instances(instances)
         boto_connection.release_address(ip)
         return "vm at "+ ip +" removed"
